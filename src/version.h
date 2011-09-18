ae4b310cfb07b135df36fa219095974348a8e5bf
WangBin <wbsecg1@gmail.com>
2011-09-18 15:03:36 +0800

    version 1.4.0

--- src/version.h
+++ src/version.h
@@ -42,7 +42,7 @@ struct version {
 
 #define APP_NAME "qop"
 
-#define APP_VERSION 0x00020e
+#define APP_VERSION 0x010400
 
 #define MAJOR ((APP_VERSION&0xff0000)>>16)
 #define MINOR ((APP_VERSION&0xff00)>>8)
