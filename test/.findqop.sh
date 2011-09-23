7570612dbd8e07a01bcb519843476239df1eb00a
WangBin <wbsecg1@gmail.com>
2011-09-23 19:50:39 +0800

    Find the right qop executable

--- test/.findqop.sh
+++ test/.findqop.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cat <<EOF
 Test scripts for qop.
 Wang Bin <wbsecg1@gmail.com>
@@ -5,10 +6,34 @@ Wang Bin <wbsecg1@gmail.com>
 
 EOF
 
+error()
+{
+  echo -e "\e[1;31merror:\e[m $1"
+  exit 1
+}
+
+
 export PATH=../bin:$PATH
+
+if [ -n "`uname -a |grep -E 'MINGW|CYGWIN'`" ];then
+  qop="qop.exe"
+  type -p $qop 2>/dev/null
+  [ $? -ne 0 ] && qop="qop_win32.exe";
+elif [ -n "`uname -a |grep 'Linux'`" ];then
+  qop="qop"
+  [ $? -ne 0 ] && qop="qop_linux"
+  type -p $qop 2>/dev/null
+  [ $? -ne 0 ] && qop="qop_linux_llvm"
+fi
+
+echo "$qop"
+type -p $qop 2>/dev/null
+[ $? -ne 0 ] && error "qop not found. Please set the PATH"
+
+:<<COMMENT
 qop="qop"; type -p $qop 2>/dev/null
 [ $? -ne 0 ] && qop="qop.exe"; type -p $qop 2>/dev/null
 [ $? -ne 0 ] && qop="qop-maemo5"; type -p $qop 2>/dev/null
 [ $? -ne 0 ] && qop="qop-ezx"; type -p $qop 2>/dev/null
 [ $? -ne 0 ] && echo 'qop not found. Please set the PATH' && exit
-
+COMMENT
