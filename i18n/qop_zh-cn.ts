7143f541bcf3ffef2a6bc8bcb58ee6e898b156d4
WangBin <wbsecg1@gmail.com>
2011-09-23 20:11:02 +0800

    translations

---
+++ i18n/qop_zh-cn.ts
@@ -0,0 +1,132 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>Archive::QArchive</name>
+    <message>
+        <source>Calculating...</source>
+        <translation type="obsolete">正在计算...</translation>
+    </message>
+</context>
+<context>
+    <name>EZProgressDialog</name>
+    <message>
+        <source>EZProgressDialog</source>
+        <translation>EZProgressDialog</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>(De)Compress Indicator</source>
+        <translation type="obsolete">(解)压缩指示器</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <source>Hide</source>
+        <translation>隐藏</translation>
+    </message>
+    <message>
+        <source>Compression/Extraction progress dialog</source>
+        <translation>压缩解压对话框</translation>
+    </message>
+    <message>
+        <source>Size: </source>
+        <translation>大小: </translation>
+    </message>
+    <message>
+        <source>Processed: </source>
+        <translation>已处理: </translation>
+    </message>
+    <message>
+        <source>Speed: </source>
+        <translation>速度: </translation>
+    </message>
+    <message>
+        <source>Elapsed: %1s Remaining: %2s</source>
+        <translation>用时: %1s 剩余: %2s</translation>
+    </message>
+    <message>
+        <source>Finished: </source>
+        <translation>已完成: </translation>
+    </message>
+    <message>
+        <source>files</source>
+        <translation>个文件</translation>
+    </message>
+    <message>
+        <source>Calculating...</source>
+        <translation>正在计算...</translation>
+    </message>
+    <message>
+        <source>Pause</source>
+        <translation>暂停</translation>
+    </message>
+    <message>
+        <source>Ratio: </source>
+        <translation type="unfinished">压缩率: </translation>
+    </message>
+</context>
+<context>
+    <name>QOutParser</name>
+    <message>
+        <source>Elapsed: %1s Remaining: %2s</source>
+        <translation>用时: %1s 剩余: %2s</translation>
+    </message>
+    <message>
+        <source>Size: </source>
+        <translation type="obsolete">大小: </translation>
+    </message>
+    <message>
+        <source>Processed: </source>
+        <translation type="obsolete">已处理: </translation>
+    </message>
+    <message>
+        <source>Speed: </source>
+        <translation>速度: </translation>
+    </message>
+    <message>
+        <source>Elapsed: %1s Processed: %2 files</source>
+        <translation type="obsolete">用时: %1s 已处理: %2 个文件</translation>
+    </message>
+    <message>
+        <source>Ratio: </source>
+        <translation type="obsolete">压缩率: </translation>
+    </message>
+    <message>
+        <source>Password Error!</source>
+        <translation>密码错误!</translation>
+    </message>
+    <message>
+        <source>Finished: </source>
+        <translation>已完成: </translation>
+    </message>
+    <message>
+        <source>files</source>
+        <translation>个文件</translation>
+    </message>
+    <message>
+        <source>Elapsed time: </source>
+        <translation>用时: </translation>
+    </message>
+    <message>
+        <source>Compressed: </source>
+        <translation>已压缩: </translation>
+    </message>
+    <message>
+        <source>Compression ratio: </source>
+        <translation>压缩率: </translation>
+    </message>
+    <message>
+        <source>Calculating...</source>
+        <translation>正在计算...</translation>
+    </message>
+    <message>
+        <source>Counting...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
